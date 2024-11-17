161c8b13e39c5e0241ac832759c5538bac775e44
Donghyeon Lee <dong97338@gmail.com>
2024-11-17 15:36:03 +0900

    env

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 import streamlit as st
 from predibase import Predibase, FinetuningConfig, DeploymentConfig
+from dotenv import load_dotenv
+import os
 
-# Initialize Predibase API client
-pb = Predibase(api_token='pb_Fb-7N9dsU-uWpNtTjBgOBw')  # kairos
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the API token from environment variables
+api_key = os.getenv('PREDIBASE_API_TOKEN')
+
+# Initialize Predibase API client with the API token
+pb = Predibase(api_token=api_key)
 
 # Streamlit app UI
 st.title("AI Teacher Assistant")
@@ -10,22 +18,16 @@ st.title("AI Teacher Assistant")
 # Input fields for user customization
 adapter_id = st.text_input("Enter adapter ID:", value="조교ai/1")
 deployment_id = st.text_input("Enter deployment ID:", value="solar-pro-preview-instruct")
-api_key = st.text_input("Enter API Key:", value="pb_Fb-7N9dsU-uWpNtTjBgOBw")
-# Max new tokens input (now configurable)
+api_key = st.text_input("Enter API Key:", value=api_key)
 max_new_tokens = st.slider("Select max new tokens:", 1, 4096, 100)
-# Temperature input (now configurable)
 temperature = st.slider("Select temperature:", 0.0, 1.0, 0.1)
-# User input area for code or question
 user_input = st.text_area("Enter your question:")
 
 # Function to generate a response using Predibase
 def generate_response(user_input, deployment_id, adapter_id, max_new_tokens, temperature, api_key):
     try:
-        pb = Predibase(api_token='pb_Fb-7N9dsU-uWpNtTjBgOBw')  # kairos
-        # Create deployment client with the provided deployment_id
+        pb = Predibase(api_token=api_key)
         lorax_client = pb.deployments.client(deployment_id)
-        
-        # Send user input to the Predibase model and get the generated response
         response = lorax_client.generate(user_input, adapter_id=adapter_id, max_new_tokens=max_new_tokens, temperature=temperature)
         return response.generated_text.split(":")[1]
     except Exception as e:
